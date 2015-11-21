b178c2d5a147b6e2fd9414b08625af23550bb11c
Samuel Baquero <sabame360@hotmail.com>
2015-11-21 17:31:25 -0500

    Lista de estaciones y boton de reserva
    añadida la lista de estaciones y boton al metodo de realizar reserva de
    vcubs.

--- app/src/main/java/co/edu/uniandes/isis2503/tbc/movilapp/ReservaVcubActivity.java
+++ app/src/main/java/co/edu/uniandes/isis2503/tbc/movilapp/ReservaVcubActivity.java
@@ -6,8 +6,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class ReservaVcubActivity extends AppCompatActivity {
+
+    ListView estacionesLV;
+    List<String> estacionesInfo;
+    String selected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +25,28 @@ public class ReservaVcubActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reserva_vcub);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, estacionesInfo);
+        estacionesLV = (ListView)findViewById(R.id.lista_estaciones);
+        estacionesLV.setAdapter(adapter);
+        estacionesLV.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        selected = (String) parent.getItemAtPosition(position);
+                    }
+                }
+        );
+        //Add Actionlisteners to Buttons
+        Button confirmarReserva= (Button) findViewById(R.id.confirmar_reserva);
+        confirmarReserva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {metodoTemporal();
+            }
+        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    
+    public void metodoTemporal(){
 
+    }
 }
