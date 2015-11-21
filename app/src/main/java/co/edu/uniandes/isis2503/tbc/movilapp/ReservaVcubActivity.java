5ede767e05b111a937cd316a8eb32f30268e0e65
Carlos Felipe Agudelo <cf.agudelo12@uniandes.edu.co>
2015-11-21 17:32:12 -0500

    Se trata de poner reserva

--- app/src/main/java/co/edu/uniandes/isis2503/tbc/movilapp/ReservaVcubActivity.java
+++ app/src/main/java/co/edu/uniandes/isis2503/tbc/movilapp/ReservaVcubActivity.java
@@ -4,13 +4,25 @@ import android.app.ListActivity;
 import android.app.LoaderManager;
 import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.SimpleCursorAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ReservaVcubActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
@@ -22,10 +34,7 @@ public class ReservaVcubActivity extends ListActivity implements LoaderManager.L
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserva_vcub);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
         //get the station list from the tbc server.
-
-
     }
 
     @Override
@@ -41,5 +50,78 @@ public class ReservaVcubActivity extends ListActivity implements LoaderManager.L
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
+    }
+
+    public class ReservaVcubTask extends AsyncTask<String[], Void, String> {
+
+        private final String LOG_TAG = ReservaVcubTask.class.getSimpleName();
+
+        @Override
+        protected String doInBackground(String[]... params) {
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String userJsonStr = null;
+
+            try {
+
+                URL url = new URL("192.168.0.5:9000/"+params[0]+"/usuario/"+params[1]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("PUT");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                userJsonStr = buffer.toString();
+
+                Log.v(LOG_TAG, "Forecast string: " + userJsonStr);
+            } catch (Exception e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final Exception e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                JSONObject jsonObject = new JSONObject(userJsonStr);
+                jsonObject.getInt("vcubsEnUso");
+                return "Tienes "+jsonObject.getInt("vcubsEnUso")+" vcubs en uso.";
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            // This will only happen if there was an error getting or parsing the forecast.
+            return null;
+        }
     }
 }
